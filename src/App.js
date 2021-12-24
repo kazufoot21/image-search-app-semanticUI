83be34f8024a042fdce66b176a1b4d11abdd98f5
kazufoot21 <kazufoot21@outlook.jp>
2021-12-24 11:29:11 +0900

    create imagesearch func

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import axios from 'axios';
 import { SearchBar } from './components/SerachBar';
+import { useState } from 'react';
+import { ImageList } from './components/ImageList';
 
 function App() {
-  const onSearchSubmit = (term) => {
-    console.log(term);
+  const [images, setImages] = useState([]);
+  const onSearchSubmit = async (term) => {
+    try {
+      const params = {
+        key: '24958346-b37558ba5f8aa3d534e1ebeae',
+        q: term,
+      };
+      const response = await axios.get('https://pixabay.com/api', { params });
+      setImages(response.data.hits);
+      if (response.data.total === 0) alert('お探しの画像はありません。');
+    } catch {
+      alert('画像の取得に失敗しました');
+    }
   };
 
   return (
     <div className="ui container" style={{ marginTop: '20px' }}>
       <SearchBar onSubmit={onSearchSubmit} />
+      <ImageList images={images} />
     </div>
   );
 }
---
+++ src/components/ImageList.js
@@ -0,0 +1,16 @@
+export const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return (
+      <a
+        href={image.pageURL}
+        key={image.id}
+        target="_blank"
+        rel="noopener mpreferrer"
+        className="ui medium image"
+      >
+        <img src={image.webformatURL} alt={image.tags} />
+      </a>
+    );
+  });
+  return <div>{images}</div>;
+};
