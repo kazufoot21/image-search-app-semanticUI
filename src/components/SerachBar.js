dd6942add2db69d1c79dac5b04bd6f12ccd042cf
kazufoot21 <kazufoot21@outlook.jp>
2021-12-24 10:38:32 +0900

    create inputFrom

---
+++ src/components/SerachBar.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export const SearchBar = (props) => {
+  const { onSubmit } = props;
+  const [term, setTerm] = useState('');
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(term);
+  };
+
+  return (
+    <form onSubmit={onFormSubmit} className="ui form">
+      <div className="field">
+        <label>Image Search</label>
+        <input
+          type="text"
+          name="search"
+          placeholder=""
+          value={term}
+          onChange={(e) => {
+            setTerm(e.target.value);
+          }}
+        />
+      </div>
+    </form>
+  );
+};
